64f9e9273005f6fb7c288707b9ca0fbf7e673ea6
kamlauka <kamlauka@gmail.com>
2024-01-19 18:16:59 +0100

    RandomColor example

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
-import EsignatureApp from './projects/01-e-signature-app/EsignatureApp';
+// import EsignatureApp from './projects/01-e-signature-app/EsignatureApp';
+import RandomizeColors from './projects/02-e-randomize-colors/randomizeColors';
 
 function App() {
   return (
-    <div className="App">
-      <EsignatureApp/>
+    <div className='App'>
+      {/* <EsignatureApp/> */}
+      <RandomizeColors/>
     </div>
   );
 }
--- src/projects/components/Title.jsx
+++ src/projects/components/Title.jsx
@@ -2,6 +2,8 @@ import React from 'react'
 
 export default function Title({classes, text}) {
   return (
-    <h1 className={!classes ? "title text-center" : classes}>{!text ? "Title" : text}</h1>
+    <h1 className={!classes ? 'title text-center' : classes && 'title text-center'}>
+      {!text ? 'Title' : text}
+    </h1>
   )
 }
